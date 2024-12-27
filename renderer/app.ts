f6a3722e4fb4a933bc942a63dcff01247b9f1fa8
Ilson Nóbrega <hello@ilson.dev>
2024-12-27 12:42:40 -0300

    feat: ajuste para atender aos requisitos do novo boilerplate

--- renderer/app.ts
+++ renderer/app.ts
@@ -1,7 +1,17 @@
 import { createSSRApp, defineComponent, h } from 'vue'
+import Aura from '@primevue/themes/aura';
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
+
+import '../src/assets/styles.scss';
+import '../src/assets/tailwind.css';
+
 import PageShell from './PageShell.vue'
 import { setPageContext } from './usePageContext'
 import type { Component, PageContext, PageProps } from './types'
+
+
 
 export { createApp }
 
@@ -21,6 +31,17 @@ function createApp(Page: Component, pageProps: PageProps | undefined, pageContex
   })
 
   const app = createSSRApp(PageWithLayout)
+
+  app.use(PrimeVue, {
+      theme: {
+          preset: Aura,
+          options: {
+              darkModeSelector: '.app-dark'
+          }
+      }
+  });
+  app.use(ToastService);
+  app.use(ConfirmationService);
 
   // Make pageContext available from any Vue component
   setPageContext(app, pageContext)
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import vue from '@vitejs/plugin-vue'
 import ssr from 'vite-plugin-ssr/plugin'
+import { fileURLToPath, URL } from 'node:url'
 import { UserConfig } from 'vite'
 
 const config: UserConfig = {
-  plugins: [vue(), ssr()]
+  plugins: [vue(), ssr()],
+  resolve: {
+    alias: {
+        '@core': fileURLToPath(new URL('./src', import.meta.url)),
+        '@pages': fileURLToPath(new URL('./pages', import.meta.url))
+    }
+}
 }
 
 export default config
